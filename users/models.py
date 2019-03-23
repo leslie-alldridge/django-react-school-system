6c7ebc7575cc74504909b0d90569b360ec753840
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-24 07:56:13 +1300

    users

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField()
+    is_teacher = models.BooleanField() 
+
+    def ___str___(self):
+        return self.username
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def ___str___(self):
+        return self user.username

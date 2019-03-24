e6d18a9f1596119517e8d5acbcbf9b36fba45716
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-24 13:51:33 +1300

    token

--- users/serializers.py
+++ users/serializers.py
@@ -39,3 +39,22 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.save()
         adapter.save_user(request, user, self)
         return user
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    user_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Token
+        fields = ('key', 'user', 'user_type')
+
+    def get_user_type(self, obj):
+        serializer_data = UserSerializer(
+            obj.user
+        ).data
+        is_student = serializer_data.get('is_student')
+        is_teacher = serializer_data.get('is_teacher')
+        return {
+            'is_student': is_student,
+            'is_teacher': is_teacher
+        }
